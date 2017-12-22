86df182662b8e92db61f3eac89f8c8fb83e040fe
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2017-12-22 16:41:15 -0700

    fourth commit

--- sim2/src/components/Login.js
+++ sim2/src/components/Login.js
@@ -1,18 +1,21 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
 
-export default class Login extends Component{
-    render(){
-        return(
-            <div>
-                <img src="../../assets/auth_logo.png" alt="authlogo"/>
-                <h2>Username</h2>
-                <input placeholder='Username'/>
-                <h2>Password</h2>
-                <input placeholder='Password'/>
-                <br/>
-                <button>Login</button>
-                <Link to='/home'><button>Register</button></Link>
+export default class Login extends Component {
+    render() {
+        return (
+            <div className='login-page'>
+                <section className='background-white'>
+                    <img src="../../assets/auth_logo.png" alt="authlogo" className='logo' />
+                    <h2 className='username'>Username</h2>
+                    <input className='username-input' placeholder='Username' />
+                    <h2 className='password'>Password</h2>
+                    <input className='password-input' placeholder='Password' />
+                    <br />
+                    <button className='login-button' >Login</button>
+                    <Link to='/home'><button className='register-button'>Register</button></Link>
+                </section>
             </div>
         )
     }
